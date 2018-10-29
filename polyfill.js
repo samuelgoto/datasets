2d1eafb07ec9fb60f65ace908ff8c05267bc466b
samuelgoto@gmail.com <samuelgoto@gmail.com>
2018-10-29 11:47:30 -0700

    fixes a bug with relative urls

--- polyfill.js
+++ polyfill.js
@@ -325,8 +325,9 @@ class Gallery extends React.Component {
               <GridList cellHeight={160} className={classes.gridList} cols={5}>
               {
                 (clazz.images || []).map((image, index) => {
-                  console.log(clazz);
-                  let url = typeof image == "string" ? image : new URL(image.url, clazz.url);
+                  // console.log(clazz.url);
+                  let url = typeof image == "string" ? new URL(image, clazz.url) : new URL(image.url, clazz.url);
+                  
                   let key = `${image.name}-${index}-${url}`;
                   return (
                     <GridListTile key={key}>
